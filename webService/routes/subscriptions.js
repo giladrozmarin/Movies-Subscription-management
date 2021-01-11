1fac67d666c13fee528bb24b8e4eda0b26237721
giladrozmarin <19gilad@gmail.com>
2021-01-11 13:51:36 +0200

    get by id

--- webService/routes/subscriptions.js
+++ webService/routes/subscriptions.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router();
 const Subscriptions  = require('../model/SubscriptionsModel')
-
-
+/*
+id: '5ffaffd0022064562cf18017',
+id_subs: '5ffb0333886d4e5c0f0f1df2'
+*/
 router.route('/').
+//Get
     get(function(req,resp)
     {
         Subscriptions.find({},  (err,subs) =>
@@ -15,15 +18,53 @@ router.route('/').
             return resp.json(subs)
         })       
     });
-    router.route('/:id').
-put(function(req,resp)
+//Get byID
+router.route('/:id').
+    get(function(req,resp)
+    {
+        Subscriptions.findById(req.params.id, function(err,subs)
+        {
+            if(err)
+            {
+                return resp.send(err)
+            }
+            return resp.json(subs)
+        })       
+    });
+
+//POST (create)
+router.route('/'). 
+post(function(req,resp)
+            {
+         
+                const s = new Subscriptions({
+                    MemberId : req.body.MemberId,
+                    Movies : [] 
+                 
+                });
+    
+                s.save(function(err,subs) {
+                    if(err)
+                    {
+                        return resp.send(err);
+                    }
+                    else
+                    {  
+                        return resp.send(subs._id) 
+                    }
+                })       
+              
+            });  
+
+router.route('/:id').
+//Update    
+    put(function(req,resp)
         {
  
             console.log(req.body)
             Subscriptions.findByIdAndUpdate(req.params.id,
             {
-               
-                
+           
              $push:{Movies:{movieId:req.body.movieId,date:req.body.date}}
             }, function(err)
             {
@@ -39,9 +80,34 @@ put(function(req,resp)
             }) 
           
         });
-//DELETE  
+router.route('/').
+//Update    
+    put(function(req,resp)
+                {
+         
+                    
+                    Subscriptions.findByIdAndUpdate(req.body._id,
+                    {
+                    
+                        Movies:req.body.Movies
+                     
+                    }, function(err)
+                    {
+                        if(err)
+                        {
+                            return resp.send(err);
+                        }
+                        else
+                        {
+                            return resp.send(req.Movies);
+                        }
+        
+                    }) 
+                  
+                });
+//Delete  
 router.route('/:id').
- delete(function(req,resp)
+    delete(function(req,resp)
     {
    
         Subscriptions.findByIdAndDelete(req.params.id,function(err)
@@ -57,4 +123,8 @@ router.route('/:id').
        }) 
     
     });
+
+
+
+
 module.exports = router; 
