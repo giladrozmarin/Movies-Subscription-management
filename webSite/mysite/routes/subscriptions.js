bafea202a8f4ac25085b5a1e32aa13faa2d43058
giladrozmarin <19gilad@gmail.com>
2021-01-11 13:55:24 +0200

    fix Create

--- webSite/mysite/routes/subscriptions.js
+++ webSite/mysite/routes/subscriptions.js
@@ -23,44 +23,13 @@ router.get('/', async (req, res, next) => {
   });
   router.post('/addMovie', async (req, res, next) => {
 
-    //data1
-    //{ movie: '5ff97c49eeb2ff64b8408090', date: '2021-01-12' }
     let data1 =  JSON.parse(JSON.stringify(req.body))
-    debugger;
-
-    //new_movie
-    //{ name: '', id: '5ff97c49eeb2ff64b8408090' }
     let new_movie ={name:"" ,id: data1.movie}
-    console.log(`data1`)
-    console.log(data1)
-    console.log(`new_movie`)
-    console.log(new_movie)
-    // id : { id: '5ff97ff248bec968ce2a682b' }
     let id =  {id: JSON.parse(req.query.obj)._ids}
-    console.log(`id`)
-    console.log(id)
-
-     /* //data2
-    {
-      _id: '5ff97c1f84179463f5fe94a1',
-      Name: 'GT1 Graham',
-      Email: 'Sincere@april.biz',
-      City: 'Gwenborough',
-      __v: 0,
-      movies: [
-        {
-          Name: 'Person1',
-          id: '5ff97c49eeb2ff64b8408090',
-          date: '2021-01-14T00:00:00.000Z'
-        },....
-      */
     let data2 =JSON.parse(req.query.obj);
 
-    console.log(`data2`)
-    console.log(data2._id)
     let a = {id: JSON.parse(req.query.obj)._id}
-    console.log(`a`)
-    console.log(a)
+
     //add movie
     await subscriptionsBL.addMovie(data1,id)
       //current movie list 
@@ -114,7 +83,9 @@ router.post('/create', async (req, res) =>
    }
    else{
    
-    memberBL.addMember(data)
+    let MemberId = await memberBL.addMember(data)
+    console.log(MemberId)
+    await subscriptionsBL.createSubscribe(MemberId)
     res.redirect('/subscriptions')
 
    }}
@@ -125,7 +96,7 @@ router.get('/delete/:id/:ids' , async (req,res) =>{
   console.log(req.params.id)
   console.log(req.params.ids)
   await memberBL.delete(req.params.id)
-  await subscriptionsBL.delete(req.params.id)
+  await subscriptionsBL.delete(req.params.ids)
   res.redirect('/subscriptions')
 } ) 
   module.exports = router;
