18ea66dfb2308b88a46718f97cba581154bcf71d
giladrozmarin <19gilad@gmail.com>
2021-01-11 13:53:45 +0200

    getSubscriptionsById updateSubscriptionsMoviesDelete

--- webSite/mysite/DAL/subscriptionsDAL.js
+++ webSite/mysite/DAL/subscriptionsDAL.js
@@ -7,8 +7,20 @@ exports.getSubscriptions = ()=> {
     return axios.get(`http://localhost:3001/subscriptions`)
 
 }
+exports.getSubscriptionsById = (id)=> {
 
 
+    return axios.get(`http://localhost:3001/subscriptions/${id}`)
+
+}
+exports.addSubscriptions = (id)=> {
+
+
+     axios.post(`http://localhost:3001/subscriptions/`,{
+         MemberId: id
+     })
+
+}
 
 exports.updateSubscriptions = (data,id)=> {
    
@@ -17,6 +29,17 @@ exports.updateSubscriptions = (data,id)=> {
     return axios.put(`http://localhost:3001/subscriptions/${id.id}`,
     {movieId: data.movie, date: data.date }
     )
+}
+exports.updateSubscriptionsMoviesDelete = (data)=> {
+   
+ 
+   console.log(data)
+    return axios.put(`http://localhost:3001/subscriptions/`,
+    {
+        _id: data._id,
+        Movies: data.Movies
+    }
+    ).then(console.log(x => x))
 }
 
 exports.deleteSubscriptionsById = (id) => {
